a01b4f2b21f84b25edb4b2e7d67a981061340b57
abdahad9 <abdahad9@gmail.com>
2025-06-12 02:48:29 +0500

    studio config

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'sanity'
 import { deskTool } from 'sanity/desk'
 import { visionTool } from '@sanity/vision'
-import { schemaTypes } from './studio/schemaTypes'
+import { schemaTypes } from './studio/schemaTypes/index'
 
 export default defineConfig({
   name: 'default',
